8fea780b0ad456f2fc1d02c4bdba96c359078437
Daniel <daniel.bedoya@socialtic.org>
2025-10-24 09:24:50 -0600

    Update DeeplProvider.ts

--- src/providers/DeeplProvider.ts
+++ src/providers/DeeplProvider.ts
@@ -15,7 +15,7 @@ export default class DeeplProvider extends ProviderBase {
   }
 
   async translate(text: string, lang: string): Promise<string[]> {
-    const l: string[] = lang.split('-')
+    const l: string[] = lang.split('|')
     const result: TextResult = await this.translator.translateText<string>(
       text, l[0] as SourceLanguageCode, l[1] as TargetLanguageCode
     )
